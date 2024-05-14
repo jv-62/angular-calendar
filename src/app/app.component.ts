e5a627ab2660cb12c7b2ff816ddef535b4501d32
Jayam Verma <jayam6297@gmail.com>
2024-05-14 18:22:16 +0530

    files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {CalendarComponent} from './calendar/calendar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalendarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-calendar';
+  meetings = {
+    '2024-04-05': ['Dring Coffee', 'Learn React', 'Sleep'],
+    '2024-04-06': ['Dring Coffee', 'Learn Angular', 'Sleep'],
+  };
 }
